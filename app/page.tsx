f6c2bd27d0ca0a4daf572802df8c26a04abbce88
Hisoka <gintama2009@hotmail.ca>
2023-10-21 22:35:26 +0000

    added intro and started research

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,90 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="flex min-h-screen flex-col items-center p-24 bg-gray-200 text-slate-900 dark:bg-slate-800 dark:text-gray-200">
+      <h1 className="text-4xl text-purple-700 dark:text-cyan-200">
+        Case Study: How UI/UX Can Help The Elderly
+      </h1>
+
+      <p className="m-2 text-lg">first_name last_name student_id</p>
+      <p className="mb-4 text-lg">first_name last_name student_id</p>
+
+      <div className="w-2/3">
+        <h2 className="m-10 mb-4 ml-0 text-3xl text-purple-700 dark:text-cyan-200">
+          Introduction
+        </h2>
+
+        <p className="indent-12 text-lg p-4 pl-0">
+          As the field of medicine advances, the average life expectancy goes
+          up, and just in the past 60 years or so, the average life expectancy
+          has gone up by over 10 years. [1] As a result, the elderly population
+          is growing, and can be expected to continue growing, but they face a
+          growing number of issues.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+
+        <p className="indent-12 text-lg p-4 pl-0">
+          Around 1 in 5 elderly Canadians feel lonely, and this trend only gets
+          worse as one ages. [2] To make matters worse, covid-19 exacerbated
+          these issues during lockdowns. [3] Loneliness is also linked to
+          psychological and physical problems such as heart disease, lung
+          disease, depression, anxiety, and much more. Another major issue that
+          the elderly face are disabilities. In Canada, 43% of of elderly (65+
+          years old) were reported to have a disability, and this only worsens
+          with age -- 73% of elderly above the age of 85 years have a
+          disability. [5]
+        </p>
+
+        <p className="indent-12 text-lg p-4 pl-0">
+          It isn&apos;t all gloom though. Technology has been advancing rapidly,
+          and this has given rise to assistive technology. Furthermore, more and
+          more of the elderly population have been using technologies such as
+          smartphones, and tablets; Pew research has reported an increase in
+          smartphone ownership from 13% up to 61% from 2012 to 2021 amongst the
+          elderly population. [6] Phone applications have great potential to
+          enrich all of our lives, but this applies especially to the elderly,
+          and that is what we sought out to explore.
+        </p>
+
+        <p className="indent-12 text-lg p-4 pl-0">
+          appname is a mobile application that seeks to solve some of the issues
+          we outlined above. appname allows an elderly person to register onto
+          the app, and for their caretakers and loved ones to also register
+          through the app so you can keep an eye out for those you care. The app
+          reminds users to take medications, exercise, and eat regularly. The
+          app also allows family check-ins via video calls, live-tracking, and
+          the app can also alert family members if medications are missed or if
+          there are irregularities in activities.
+        </p>
+
+        <p className="indent-12 text-lg p-4 pl-0">
+          appname was also designed with accessibility in mind. Features such as
+          larger text, text-to-speech, and good contrast ratio allows those who
+          are visually impaired to interact with the app more easily.
+          Furthermore, Usability is prioritized by using UI/UX design patterns
+          such as minimalism, and simplified navigation.
+        </p>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="w-2/3 mt-6">
+        <h2 className="m-10 mb-4 ml-0 text-3xl text-purple-700 dark:text-cyan-200">
+          Research
+        </h2>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <p className="indent-12 text-lg p-4 pl-0">
+          Let us now turn our focus to what the current research says. We delved
+          into some of the research in the introduction, but we wanted to gain a
+          better understanding of our users, and we also wanted to look at the
+          current mobile applications available on the market.
+        </p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <p className="indent-12 text-lg p-4 pl-0">
+          Around 14% of seniors (age 65 and up) in Canada live in some kind of
+          long-term care home. [7] Statcan data also reports many older people
+          living alone -- up to 36.6% of Canadian women over the age of 85 live
+          alone, and up to 21.8% of Canadian men over the age of 85 live alone.
+        </p>
       </div>
     </main>
-  )
+  );
 }
